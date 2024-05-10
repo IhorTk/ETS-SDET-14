70730e5942d21e564b639beefcb2861244d596a3
IgorTkachenko <igortkachenko987@gmail.com>
2024-05-10 20:08:15 +0200

    Add file main.java

---
+++ untitled/src/Hausaufgabe/jfgdjks.java
@@ -0,0 +1,39 @@
+package Hausaufgabe;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class jfgdjks {
+//    public static void main(String[] args) {
+////        System.out.println(Math.E);
+////        System.out.println(Math.PI);
+//
+//        for (int i = 0; i < 10; i++) {
+//            int h = (100+(int)(Math.random()*301));
+//            System.out.print(" " + h);
+//        }
+//        System.out.println();
+//
+//        int bb = 95;
+//        boolean cc = false;
+//
+//        System.out.println("squirrelPlay(bb,cc) = " + squirrelPlay(bb, cc));
+//
+//    }
+////    public static boolean squirrelPlay(int speed, boolean isBirthday) {
+//        int bb = 0;
+
+//        if(isBirthday == false && speed<=60){
+//            bb = 0;
+//        } else if (isBirthday == false &&(speed>=61 && speed<=80)){
+//            bb = 1;
+//        }else if (isBirthday == false && speed>81){
+//            bb = 2;
+//        }else{
+//            bb = 0;
+//        }
+//        return bb;
+//    }
+//
+}
