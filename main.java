eec44b7dcd22f5b270d6035b0af7be21f8be6d2a
IgorTkachenko <igortkachenko987@gmail.com>
2024-05-13 20:55:13 +0200

    New

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ public class mentor {
         double doubleN = 12.4;
         booleanN = false;
         doubleN = 15.9;
+        doubleN1 = 165.9;
         System.out.println(booleanN);
         System.out.println(doubleN);
 
